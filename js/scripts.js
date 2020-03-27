bf48a69ab780ef597e0c26481632cba7c3173f4f
Rizoma <rizomadg@gmail.com>
2020-03-27 15:30:00 -0300

    Home listo (incluido responsive)

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  $('#slider-home').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+
+});
